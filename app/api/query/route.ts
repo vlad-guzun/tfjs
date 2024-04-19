0d43d360a2a06f1db0b2a9629de7d1c0ef5f42ba
vlad-guzun <vladguzun20022@gmail.com>
2024-04-20 01:45:20 +0300

    sters dynamic sus

--- app/api/query/route.ts
+++ app/api/query/route.ts
@@ -1,6 +1,5 @@
 import { MongoClient } from "mongodb";
 import { NextRequest, NextResponse } from "next/server";
-export const dynamic = 'force-dynamic';
 
 export async function GET(req: NextRequest) {
     try {
