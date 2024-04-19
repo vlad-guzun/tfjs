623e44354308dd4cc23d7d5b72088148aaf1b935
vlad-guzun <vladguzun20022@gmail.com>
2024-04-20 01:59:55 +0300

    cu force dyn

--- app/api/query/route.ts
+++ app/api/query/route.ts
@@ -1,5 +1,6 @@
 import { MongoClient } from "mongodb";
 import { NextRequest, NextResponse } from "next/server";
+export const dynamic = 'force-dynamic';
 
 export async function GET(req: NextRequest) {
     try {
