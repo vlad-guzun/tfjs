2ceb82c2e23f66fde03eb42b6db0da0c230d1e66
vlad-guzun <vladguzun20022@gmail.com>
2024-04-28 13:23:29 +0300

    salvat in db embedded_user

--- app/api/save_to_db_embedded_user/route.ts
+++ app/api/save_to_db_embedded_user/route.ts
@@ -3,22 +3,16 @@ import EmbeddedUser from "@/lib/database/models/embeddedUser";
 import {  NextResponse } from "next/server";
 
 export async function POST(req: Request){
-    const {embedding_data} = await req.json();
+    const {clerkId,embedding_data} = await req.json();
+    
 
     const embeddedInterests = embedding_data[0];
     const embeddedLocation = embedding_data[1];
     const embeddedReasonForJoining = embedding_data[2];
 
     await connectToDatabase();
-
-    const embedded_user = await EmbeddedUser.create({
-        clerkId: "123",
-        embeddedInterests,
-        embeddedLocation,
-        embeddedReasonForJoining,
-    });
+    const embeddedUser = await EmbeddedUser.create({clerkId,embeddedInterests,embeddedLocation,embeddedReasonForJoining});
 
 
-
-    return NextResponse.json(embedded_user);
+    return NextResponse.json({embeddedUser});
 }
