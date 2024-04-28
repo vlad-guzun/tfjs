67d9683bd8a3ac2461013b800d62068eb50204df
vlad-guzun <vladguzun20022@gmail.com>
2024-04-28 13:01:11 +0300

    am uitat sa salvez in db

--- app/api/save_to_db_embedded_user/route.ts
+++ app/api/save_to_db_embedded_user/route.ts
@@ -1,10 +1,24 @@
 import { connectToDatabase } from "@/lib/database/connectToDatabase";
 import EmbeddedUser from "@/lib/database/models/embeddedUser";
-import { NextRequest, NextResponse } from "next/server";
+import {  NextResponse } from "next/server";
 
 export async function POST(req: Request){
     const {embedding_data} = await req.json();
 
+    const embeddedInterests = embedding_data[0];
+    const embeddedLocation = embedding_data[1];
+    const embeddedReasonForJoining = embedding_data[2];
 
-    return NextResponse.json(embedding_data);
+    await connectToDatabase();
+
+    const embedded_user = await EmbeddedUser.create({
+        clerkId: "123",
+        embeddedInterests,
+        embeddedLocation,
+        embeddedReasonForJoining,
+    });
+
+
+
+    return NextResponse.json(embedded_user);
 }
