c7ebdde3449c3000515b28e7c684bf76f641d247
vlad-guzun <vladguzun20022@gmail.com>
2024-05-22 19:10:19 +0300

    PRESENCE main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,12 @@
 "use client";
 import * as React from "react";
 import * as tf from "@tensorflow/tfjs";
-import * as PopoverPrimitive from "@radix-ui/react-popover";
-import { HiOutlineDotsHorizontal } from "react-icons/hi";
-import { Eye, Heart, HeartCrack } from 'lucide-react';
-import StarterModal from "@/components/StarterModal";
 import { useUser } from '@clerk/nextjs';
 import { useEffect, useState } from 'react';
 import Cookies from 'js-cookie';
 import Image from 'next/image';
-import { Skeleton } from "@/components/ui/skeleton";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { Button } from "@/components/ui/button";
-import { Plane, UserPlus, UserMinus, X, SendHorizontal } from 'lucide-react';
 import Link from "next/link";
 import * as use from "@tensorflow-models/universal-sentence-encoder";
 import { Textarea } from "@/components/ui/textarea";
@@ -21,6 +15,10 @@ import { toast } from "@/components/ui/use-toast";
 import { useRouter } from "next/navigation";
 import useActiveList from "@/hooks/useActiveList";
 import { checkActivityOfAllUsers, updateLastSeen } from "@/lib/actions/user.action";
+import { HiOutlineDotsHorizontal } from "react-icons/hi";
+import { Eye, Heart, HeartCrack, UserPlus, UserMinus, SendHorizontal } from 'lucide-react';
+import StarterModal from "@/components/StarterModal";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Home() {
   const [clerkId, setClerkId] = useState<string>();
@@ -34,14 +32,15 @@ export default function Home() {
 
   const { user, isLoaded } = useUser();
   const router = useRouter();
-  
+  const { members } = useActiveList();
+
   useEffect(() => {
     const getSession = async () => {
       if (user && isLoaded) {
         const sessions = await user.getSessions();
         console.log(sessions);
 
-        const latestSession = sessions.reduce((latest:any, session:any) => {
+        const latestSession = sessions.reduce((latest: any, session: any) => {
           if (!latest || new Date(session.lastActiveAt) > new Date(latest.lastActiveAt)) {
             return session;
           }
@@ -53,9 +52,8 @@ export default function Home() {
           const lastActiveResponse: Date = await updateLastSeen(user?.id, new Date(latestSession.lastActiveAt));
           setLastActiveAt(lastActiveResponse);
 
-          const active_users:User_with_interests_location_reason[] = await checkActivityOfAllUsers();
+          const active_users: User_with_interests_location_reason[] = await checkActivityOfAllUsers();
           setActiveUsers(active_users);
-          
         }
       }
     };
@@ -74,7 +72,6 @@ export default function Home() {
       setUserDoc(data);
     };
     checkIfModalSubmittedAndGetFullUser();
-    
   }, [clerkId]);
 
   useEffect(() => {
@@ -207,7 +204,7 @@ export default function Home() {
   };
 
   const send_feedback = async (sender_of_feedback: string | undefined, recipient_of_feedback: string, feedback_content: string) => {
-    const response = await fetch("/api/send_feedback",{
+    const response = await fetch("/api/send_feedback", {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
@@ -230,36 +227,34 @@ export default function Home() {
           color: "white",
           border: "1px slate-800 solid",
           fill: "white",
-        
         }
       })
     } else {
       console.log("eroare");
     }
-    
   }
 
   useEffect(() => {
     setFeedbackSent(false);
-  },[feedbackSent]);
+  }, [feedbackSent]);
 
   function formatLastSeen(lastSeen: Date | null): React.JSX.Element | null {
     if (!lastSeen) {
-      return <span className="text-white">long time ago</span>;
+      return <span className="text-white">inactive</span>;
     }
-  
+
     const lastSeenDate = new Date(lastSeen);
     const now = new Date();
     const diffInSeconds = Math.round((now.getTime() - lastSeenDate.getTime()) / 1000);
-    
+
     if (diffInSeconds < 60) {
-      return <span>less than a minute</span>;
+      return <span className="bg-teal-500 px-3 py-1 rounded-xl text-white">{Math.floor(diffInSeconds)}s</span>;
     } else if (diffInSeconds < 3600) {
-      return <span>{Math.floor(diffInSeconds / 60)} min</span>;
+      return <span className="bg-teal-500 px-3 py-1 rounded-xl text-white">{Math.floor(diffInSeconds / 60)}m</span>;
     } else if (diffInSeconds < 43200) {
-      return <span>{Math.floor(diffInSeconds / 3600)} h</span>;
+      return <span className="bg-teal-500 px-3 py-1 rounded-xl text-white">{Math.floor(diffInSeconds / 3600)}h</span>;
     }
-  
+
     return null;
   }
 
@@ -269,51 +264,56 @@ export default function Home() {
       {Cookies.get(`modalSubmitted_${clerkId}`) === "true" && (
         <div className="flex flex-wrap justify-center w-full">
           {recommendedUsers ? (
-            recommendedUsers.map((recommended_user, index) => (
-              <div key={index} className="mt-5 flex flex-col items-center justify-center mb-1 ">
-                <div className="flex justify-center items-center w-72 h-[140px] bg-black rounded-lg relative mb-6 mr-6">
-                  <div className="flex items-center justify-center relative">
-                    <Image src={recommended_user.photo} className="mt-4 rounded-full border-2 border-slate-700" width={150} height={150} alt="user" />
-                    {recommended_user.lastSeen && String(recommended_user.lastSeen).length > 0 ? (
-                      <div className="absolute bottom-0 right-0  text-white text-xs p-1 rounded-md">
-                        {recommended_user.lastSeen ? formatLastSeen(new Date(recommended_user.lastSeen)) : null}
-                      </div>
-                    ) : null}
-                  </div>
-                </div>
-                <Popover> {/*de schimbat in vrun icon*/}
-                  <PopoverTrigger className=" text-white px-2 py-1 rounded-2xl hover:bg-white hover:text-black mb-[150px]"><HiOutlineDotsHorizontal /></PopoverTrigger>
-                  <PopoverContent className="border-slate-800 bg-black text-white flex flex-col items-center gap-4 mt-2">
-                    <h3 className="text-center text-white mb-3">{recommended_user.username}</h3>
-                    <div className="border-slate-800 bg-black text-white flex gap-4">
-                      <Button className="w-full hover:bg-white hover:text-black bg-black border border-slate-800"><Link href={`/profile/${recommended_user.username}`}><Eye /></Link></Button>
-                      <Button
-                        className="w-full hover:bg-white hover:text-black bg-black border border-slate-800"
-                        onClick={() => followStatuses[recommended_user.clerkId] === "follow" ? handleFollow(clerkId, recommended_user.clerkId) : handleUnfollow(clerkId, recommended_user.clerkId)}
-                      >
-                        {followStatuses[recommended_user.clerkId] === "follow" ? <UserPlus /> : <UserMinus />}
-                      </Button>
-                      <div>
-                        {!Cookies.get(`feedback_${clerkId}_${recommended_user.clerkId}`) && (
-                          <Popover>
-                            <PopoverTrigger>
-                              <div className="flex gap-2 border border-slate-800 px-2 py-[6px] rounded-md hover:bg-white hover:text-black">
-                                <Heart className="text-red-800"/> <HeartCrack className="text-slate-800"/>
-                              </div>
-                            </PopoverTrigger>
-                            <PopoverContent className="bg-black text-white border border-slate-800">
-                              <Label className="text-white">What do you think about <span className="text-slate-400">{recommended_user.username}</span>?</Label>
-                              <Textarea className="bg-black text-white border border-slate-800" value={feedback} onChange={e => setFeedback(e.target.value)}/>
-                              <Button onClick={() => send_feedback(clerkId, recommended_user.clerkId, feedback)} className="ml-[200px] mt-[6px] bg-black text-white hover:text-black hover:bg-white"><SendHorizontal /></Button>
-                            </PopoverContent> 
-                          </Popover>
-                        )}
-                      </div>
+            recommendedUsers.map((recommended_user, index) => {
+              const isUserActive = members.indexOf(recommended_user.clerkId) !== -1;
+              return (
+                <div key={index} className="mt-5 flex flex-col items-center justify-center mb-1 ">
+                  <div className="flex justify-center items-center w-72 h-[140px] bg-black rounded-lg relative mb-6 mr-6">
+                    <div className="flex items-center justify-center relative">
+                      <Image src={recommended_user.photo} className="mt-4 rounded-full border-2 border-slate-700" width={150} height={150} alt="user" />
+                      {isUserActive ? (
+                        <div className="absolute top-5 right-0 w-5 h-5 bg-green-600 rounded-full pulsating-dot"></div>
+                      ) : (
+                        <div className="absolute bottom-0 right-0 text-white text-xs p-1 rounded-md">
+                          {recommended_user.lastSeen ? formatLastSeen(new Date(recommended_user.lastSeen)) : null}
+                        </div>
+                      )}
                     </div>
-                  </PopoverContent>
-                </Popover>
-              </div>
-            ))
+                  </div>
+                  <Popover>
+                    <PopoverTrigger className=" text-white px-2 py-1 rounded-2xl hover:bg-white hover:text-black mb-[150px]"><HiOutlineDotsHorizontal /></PopoverTrigger>
+                    <PopoverContent className="border-slate-800 bg-black text-white flex flex-col items-center gap-4 mt-2">
+                      <h3 className="text-center text-white mb-3">{recommended_user.username}</h3>
+                      <div className="border-slate-800 bg-black text-white flex gap-4">
+                        <Button className="w-full hover:bg-white hover:text-black bg-black border border-slate-800"><Link href={`/profile/${recommended_user.username}`}><Eye /></Link></Button>
+                        <Button
+                          className="w-full hover:bg-white hover:text-black bg-black border border-slate-800"
+                          onClick={() => followStatuses[recommended_user.clerkId] === "follow" ? handleFollow(clerkId, recommended_user.clerkId) : handleUnfollow(clerkId, recommended_user.clerkId)}
+                        >
+                          {followStatuses[recommended_user.clerkId] === "follow" ? <UserPlus /> : <UserMinus />}
+                        </Button>
+                        <div>
+                          {!Cookies.get(`feedback_${clerkId}_${recommended_user.clerkId}`) && (
+                            <Popover>
+                              <PopoverTrigger>
+                                <div className="flex gap-2 border border-slate-800 px-2 py-[6px] rounded-md hover:bg-white hover:text-black">
+                                  <Heart className="text-red-800" /> <HeartCrack className="text-slate-800" />
+                                </div>
+                              </PopoverTrigger>
+                              <PopoverContent className="bg-black text-white border border-slate-800">
+                                <Label className="text-white">What do you think about <span className="text-slate-400">{recommended_user.username}</span>?</Label>
+                                <Textarea className="bg-black text-white border border-slate-800" value={feedback} onChange={e => setFeedback(e.target.value)} />
+                                <Button onClick={() => send_feedback(clerkId, recommended_user.clerkId, feedback)} className="ml-[200px] mt-[6px] bg-black text-white hover:text-black hover:bg-white"><SendHorizontal /></Button>
+                              </PopoverContent>
+                            </Popover>
+                          )}
+                        </div>
+                      </div>
+                    </PopoverContent>
+                  </Popover>
+                </div>
+              );
+            })
           ) : (
             Array.from({ length: Math.random() * 10 }, (_, index) => (
               <div key={index} className="flex justify-center items-center w-72 h-64 bg-black rounded-lg relative mb-6 mr-6">
@@ -323,6 +323,47 @@ export default function Home() {
           )}
         </div>
       )}
+      <style jsx>{`
+  @keyframes pulsate {
+    0% {
+      transform: scale(1);
+    }
+    10% {
+      transform: scale(1.02);
+    }
+    20% {
+      transform: scale(1.04);
+    }
+    30% {
+      transform: scale(1.06);
+    }
+    40% {
+      transform: scale(1.08);
+    }
+    50% {
+      transform: scale(1.1);
+    }
+    60% {
+      transform: scale(1.08);
+    }
+    70% {
+      transform: scale(1.06);
+    }
+    80% {
+      transform: scale(1.04);
+    }
+    90% {
+      transform: scale(1.02);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
+
+  .pulsating-dot {
+    animation: pulsate 0.6s infinite;
+  }
+`}</style>
     </div>
   );
 }
